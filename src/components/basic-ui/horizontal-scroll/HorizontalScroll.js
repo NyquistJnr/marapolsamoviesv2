81bb67e634c424a93c451693f13f6b0188ae8d00
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2024-08-31 14:54:20 +0100

    Updated the Horizontal Scroll Component.

--- src/components/basic-ui/horizontal-scroll/HorizontalScroll.js
+++ src/components/basic-ui/horizontal-scroll/HorizontalScroll.js
@@ -1,78 +1,52 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 import { Container } from "react-bootstrap";
 import classes from "./HorizontalScroll.module.css";
 
-import Image from "next/image";
-import Link from "next/link";
-
 const HorizontalScroll = (props) => {
-  const router = useRouter();
-  const currentPath = usePathname();
   return (
-    <Container>
-      <main className={classes.bodyContainer}>
+    <Container fluid>
+      <div className={classes.bodyContainer}>
         {props.data?.length > 0 ? (
-          <>
-            {props.data?.map((item) => (
-              <section
-                key={item.title}
-                className="col-12 col-sm-12 col-md-6 col-lg-3"
-              >
-                <div className={classes.cardConatiner}>
-                  <Image
-                    src={item.image}
-                    alt={item.title}
-                    width={300}
-                    height={300}
-                    style={{
-                      width: "85%",
-                      height: 180,
-                      objectFit: "cover",
-                      borderRadius: 10,
-                      objectPosition: "center center",
-                    }}
-                    priority
-                    className={classes.imgStyle}
-                  />
-                  <div
-                    style={{
-                      textAlign: "left",
-                      marginTop: 15,
-                      width: "85%",
-                    }}
-                  >
-                    <Link href={`/reviews/${item.id}`}>
-                      <h4
-                        style={{
-                          fontSize: 18,
-                          marginBottom: 20,
-                          fontWeight: "bold",
-                        }}
-                      >
-                        {item.title}
-                      </h4>
-                    </Link>
-                    <p style={{ fontSize: 12, marginBottom: 0 }}>
-                      <span style={{ fontWeight: "bold" }}>Genre: </span>
-                      {item.genre}
-                    </p>
-                    <p style={{ fontSize: 12 }}>
-                      <span style={{ fontWeight: "bold" }}>
-                        Streaming Platform:{" "}
-                      </span>
-                      {item.streamingPlatform}
-                    </p>
-                  </div>
+          props.data.map((item) => (
+            <div key={item.title} className={classes.cardContainer}>
+              <div className={classes.cardContent}>
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  width={300}
+                  height={180}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    borderRadius: "10px",
+                  }}
+                  priority
+                  className={classes.imgStyle}
+                />
+                <div className={classes.textContent}>
+                  <Link href={`/reviews/${item.id}`}>
+                    <h4>{item.title}</h4>
+                  </Link>
+                  <p>
+                    <strong>Genre: </strong>
+                    {item.genre}
+                  </p>
+                  <p>
+                    <strong>Streaming Platform: </strong>
+                    {item.streamingPlatform}
+                  </p>
                 </div>
-              </section>
-            ))}
-          </>
+              </div>
+            </div>
+          ))
         ) : (
           <div>No data</div>
         )}
-      </main>
+      </div>
     </Container>
   );
 };
